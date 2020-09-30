3471571766568a27e56a43cd0198d4b50c792e70
Mostafa54225 <mostafa.draven@gmail.com>
2020-09-30 20:07:19 +0200

    Updates

---
+++ precache-manifest.b11882cb4f5b162a6ac49db2059b2ce5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d8bc04cfb464124519afbdc0b038afd",
+    "url": "/COVID-19-Tracker/index.html"
+  },
+  {
+    "revision": "e9631b47e46f9680440e",
+    "url": "/COVID-19-Tracker/static/css/main.b48cad39.chunk.css"
+  },
+  {
+    "revision": "d4f2cdbc8d6464af0777",
+    "url": "/COVID-19-Tracker/static/js/2.dbb1ccaf.chunk.js"
+  },
+  {
+    "revision": "1ef8d540b3072f991ff256dfc61c7bcd",
+    "url": "/COVID-19-Tracker/static/js/2.dbb1ccaf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9631b47e46f9680440e",
+    "url": "/COVID-19-Tracker/static/js/main.337ad169.chunk.js"
+  },
+  {
+    "revision": "9e7fa56bc891a78ef397",
+    "url": "/COVID-19-Tracker/static/js/runtime-main.f980bf8b.js"
+  }
+]);
--- src/components/index.js
+++
@@ -1,3 +0,0 @@
-export { default as Cards } from './Card/Cards';
-export { default as Charts } from './Charts/Charts';
-export { default as CountryPicker } from './CountryPicker/CountryPicker';
